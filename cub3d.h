88e8a606c09213ccf612281cf94befd74e9b3dd5
BekxFR <104771705+BekxFR@users.noreply.github.com>
2023-01-18 16:31:03 +0100

    Degree & Rayons OK

---
+++ cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chillion <chillion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 10:54:34 by mgruson           #+#    #+#             */
+/*   Updated: 2023/01/17 17:05:16 by chillion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <errno.h>
+# include <stdio.h>
+# include "libs/libft/includes/libft.h"
+# include "libs/libft/includes/ft_printf.h"
+# include "libs/libft/includes/get_next_line.h"
+# include "libs/minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <math.h>
+
+#endif
